2acbea9d7332f937473b01c97333e5dcada6cbe1
Karan Dhillon <Karanpal.dhillon@gmail.com>
2024-02-28 09:06:07 +0530

    link button

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,9 +1,14 @@
+import PropTypes from 'prop-types'
 import { Link } from "react-router-dom"
-const LinkButton = () => {
+
+const LinkButton = ({ children }) => {
   const classNames = "text-sm text-blue-500 hover:text-blue-600 hover:underline"
   return (
-    <Link className={classNames} to="/menu">&larr; Back to menu</Link>
+    <Link className={classNames} to="/menu">{children}</Link>
   )
 }
 
+LinkButton.propTypes = {
+  children: PropTypes.node
+}
 export default LinkButton
